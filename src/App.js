5308672348c945c45cf5496a0d6e09212e7a38bc
EmCarAb <carpinteiroabaloemma@gmail.com>
2024-02-26 13:21:32 +0100

    ADD componente personajes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import imagenRickMorty from "./img/rick-morty.png"
 import './App.css';
 import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
 
@@ -11,15 +12,20 @@ function App() {
     const characterApi = await api.json();
 
     setCharacter(characterApi.results);
-
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imagenRickMorty} alt="Rick y Morty" className="img-home"></img>
-        <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacter}/> 
+        ) : (
+        <>
+          <img src={imagenRickMorty} alt="Rick y Morty" className="img-home"></img>
+          <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+        </>
+        )}
       </header>
     </div>
   );
